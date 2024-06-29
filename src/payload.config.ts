98dd326d85807aeb6f4778a628d7de0825988862
mksaiyed <mrmksaiyed@gmail.com>
2024-06-29 12:23:20 +0530

    Setup custom server for payload

---
+++ src/payload.config.ts
@@ -0,0 +1,32 @@
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+})
+
+import { buildConfig } from 'payload/config'
+
+import { Pages } from './collections/Pages'
+import BeforeLogin from './components/BeforeLogin'
+
+export default buildConfig({
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || '',
+  collections: [Pages],
+  admin: {
+    bundler: webpackBundler(),
+    components: {
+      beforeLogin: [BeforeLogin],
+    },
+  },
+  editor: lexicalEditor({}),
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+})
